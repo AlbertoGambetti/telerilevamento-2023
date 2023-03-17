c574be03b8453a565bdc454d54fd3c27b9fd4138
AlbertoGambetti <127427260+AlbertoGambetti@users.noreply.github.com>
2023-03-17 17:42:30 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -76,9 +76,10 @@ plotRGB(l2011, r=4, g=3, b=2, stretch="lin")# ho assegnato la banda 4 degli infr
 plotRGB(l2011, r=3, g=4, b=2, stretch="lin")# ho assegnato la banda 4 degli infrarossi alla componente green.
 plotRGB(l2011, r=3, g=2, b=4, stretch="lin")
 
-
-
-
+#multiframe with natural and false colours
+par(mfrow=c(2,1))
+plotRGB(l2011, r=3, g=2, b=1, stretch="lin")
+plotRGB(l2011, r=3, g=2, b=4, stretch="lin")
 
 
 
