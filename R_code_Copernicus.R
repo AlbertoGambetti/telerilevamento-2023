2f71b6828bf4a70c2370b00fff02c85da0b446db
AlbertoGambetti <127427260+AlbertoGambetti@users.noreply.github.com>
2023-05-11 17:48:54 +0200

    Create R_code_Copernicus.R

---
+++ R_code_Copernicus.R
@@ -0,0 +1,12 @@
+#R code for downloading and visualizing Copernicus data
+
+#install.packages("ncdf4")
+library(ncdf4)
+library(raster)
+library(ggplot2)
+library(viridis)
+
+# Setting the working directory for macOS
+setwd("~/Documents/lab")
+
+# dataset: c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc
